fa69e957775cb81b35ad6d5ec5df1bd316629318
sailichev <sailichev@gmail.com>
2014-09-26 17:06:09 +0400

    Server via TCP/IP, first edition. Manually tested. Automated tests are one their way.

---
+++ Implementation/Autocompletion/Autocompletion.TCP.ServerEnvironment/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Autocompletion.TCP.ServerEnvironment
+{
+	using System;
+	using System.Linq;
+	using System.IO;
+
+	using Autocompletion.Core;
+
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const int resultCount = 10;
+
+			string file = args[0];
+			int port = int.Parse(args[1]);
+
+			var trie = new ItemTrie();
+
+			Console.WriteLine("Initializing data from file: {0}", file);
+			Init(trie, file, resultCount);
+
+			using (var server = new Server(port, trie))
+			{
+				Console.WriteLine("Server started at port: {0}", port);
+				Console.WriteLine("Press any key to exit");
+				Console.ReadKey(true);
+			}
+		}
+
+		private static void Init(ItemTrie trie, string file, int resultCount)
+		{
+			var testIn = File.ReadAllLines(file);
+
+			int dictionaryLength = int.Parse(testIn[0]);
+
+			var dictionaryData = testIn.
+				Skip(1).
+				Take(dictionaryLength).
+				Select(_ => _.Split(' ')).
+				Select(_ => new
+				{
+					value = _[0],
+					relevancy = int.Parse(_[1])
+				}
+				);
+
+			foreach (var a in dictionaryData)
+				trie.Add(a.value, a.relevancy);
+
+			trie.Prepare(resultCount);
+		}
+	}
+}
---
+++ Implementation/Autocompletion/Autocompletion.TCP/Server.cs
@@ -0,0 +1,109 @@
+﻿namespace Autocompletion.TCP
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Net;
+	using System.Net.Sockets;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using Autocompletion.Interfaces;
+
+
+	public class Server : IDisposable
+	{
+		public static readonly Encoding Encoding = Encoding.ASCII;
+		public static readonly string Command = "get ";
+		public static readonly string NewLine = "\r\n";
+
+		private readonly TcpListener listener;
+		private readonly ICollection<TcpClient> clients = new List<TcpClient>();
+
+		public readonly IAutocompletionSource autocompletionSource;
+
+		public Server(int port, IAutocompletionSource autocompletionSource)
+		{
+			this.autocompletionSource = autocompletionSource;
+
+			this.listener = new TcpListener(IPAddress.Any, port);
+
+			this.listener.Start();
+
+			Task.Factory.StartNew(Listen);
+		}
+
+		public void Dispose()
+		{
+			listener.Stop();
+
+			foreach (var client in clients)
+				if (client.Connected)
+					client.Close();
+
+			clients.Clear();
+		}
+
+		private void Listen()
+		{
+			do
+			{
+				try
+				{
+					var client = listener.AcceptTcpClient();
+					
+					Task.Factory.
+						StartNew(
+							() => Loop(client)
+						).ContinueWith(
+							_ => client.Close()
+						);
+					
+					clients.Add(client);
+				}
+				catch (SocketException)
+				{
+					break;
+				}
+			} while (true);
+		}
+
+		private void Loop(TcpClient client)
+		{
+			do
+			{
+				var readBuf = new byte[40]; // max <prefix> length is defined to be 15 symbols so 40 bytes is enough anyways, including the command itself
+
+				try
+				{
+					var stream = client.GetStream();
+
+					var readCount = stream.Read(readBuf, 0, readBuf.Length);
+
+					if (readCount == 0)
+						break;
+
+					var readString = Encoding.GetString(readBuf, 0, readCount);
+
+					if (readString.StartsWith(Command))
+					{
+						var prefix = readString.Substring(Command.Length);
+						var result = this.autocompletionSource.GetStrings(prefix);
+
+						var writeBuf = Encoding.GetBytes(string.Join(NewLine, result));
+						stream.Write(writeBuf, 0, writeBuf.Length);
+					}
+				}
+				catch (SocketException)
+				{
+					break;
+				}
+				catch (IOException)
+				{
+					break;
+				}
+				
+			} while (true);
+		}
+	}
+}
